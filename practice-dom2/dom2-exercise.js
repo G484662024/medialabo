8a91cdebd82eb72a7550444fb0f8dd489febd36c
G484662024 <248466@st.takushoku-u.ac.jp>
2025-07-03 16:57:31 +0900

    第５回課題完了

---
+++ practice-dom2/dom2-exercise.js
@@ -0,0 +1,38 @@
+//////////////// ここは書き換えてはいけない！ 
+
+let campus = {
+	address: "八王子市館町",
+	buildingD: ["D101", "D102", "D103", "D201", "D202", "D203", "D204", "D205"],
+	lon: 35.624869704425,
+	lat: 139.28201056633
+};
+
+let gakka = [
+	{name: "機械システム工学科", ename: "Department of Mechanical Systems Engineering"},
+	{name: "電子システム工学科", ename: "Department of Electronics and Computer Systems"},
+	{name: "情報工学科", ename: "Department of Computer Science"},
+	{name: "デザイン学科", ename: "Department of Design"}
+];
+
+//////////////// ここから下にプログラムを書きたそう!
+function show(){
+	let a=document.querySelector("h2#addr");
+	let p=document.createElement("p");
+	a.insertAdjacentElement("afterend",p);
+	p.textContent=campus.address;
+	let d=document.querySelector("h2#dept");
+	let u=document.createElement("ul");
+	d.insertAdjacentElement("afterend",u);
+	for(let g of gakka){
+
+let l =document.createElement('li');
+
+u.insertAdjacentElement('beforeend',l);
+
+l.textContent = g.name;
+
+}
+}
+let b = document.querySelector('button#show');
+b.addEventListener('click',show);
+
--- weather.js
+++ weather.js
@@ -1,20 +1,68 @@
 
 // 課題3-2 のプログラムはこの関数の中に記述すること
 function print(data) {
+  console.log(data.name);
   console.log(data.coord.lon);
   console.log(data.coord.lat);
   console.log(data.weather[0].description);
   console.log(data.main.temp_max);
   console.log(data.main.temp_min);
   console.log(data.main.humidity);
-  console.log(data.name);
-  
+  console.log(data.wind.speed);
+  console.log(data.wind.deg);
 }
 
 // 課題5-1 の関数 printDom() はここに記述すること
 function printDom(data) {
+    let d=document.querySelector("div#result");
+    d.textContent = '';
+  
+
+  let ul = document.createElement('ul');
+  
+  
+  d.insertAdjacentElement('beforeend', ul);
+  
+
+  let li1 = document.createElement('li');
+  li1.textContent = '都市名: ' + data.name;
+  ul.insertAdjacentElement('beforeend', li1);
+
+  let li2 = document.createElement('li');
+  li2.textContent = '経度: ' + data.coord.lon;
+  ul.insertAdjacentElement('beforeend', li2);
+
+  let li3 = document.createElement('li');
+  li3.textContent = '緯度: ' + data.coord.lat;
+  ul.insertAdjacentElement('beforeend', li3);
+
+  let li4 = document.createElement('li');
+  li4.textContent = '天気: ' + data.weather[0].description;
+  ul.insertAdjacentElement('beforeend', li4);
+
+  let li5 = document.createElement('li');
+  li5.textContent = '最高気温: ' + data.main.temp_max + '°C';
+  ul.insertAdjacentElement('beforeend', li5);
+
+  let li6 = document.createElement('li');
+  li6.textContent = '最低気温: ' + data.main.temp_min + '°C';
+  ul.insertAdjacentElement('beforeend', li6);
+
+  let li7 = document.createElement('li');
+  li7.textContent = '湿度: ' + data.main.humidity + '%';
+  ul.insertAdjacentElement('beforeend', li7);
+
+  let li8 = document.createElement('li');
+  li8.textContent = '風速: ' + data.wind.speed + ' m/s';
+  ul.insertAdjacentElement('beforeend', li8);
+
+  let li9 = document.createElement('li');
+  li9.textContent = '風向: ' + data.wind.deg + '°';
+  ul.insertAdjacentElement('beforeend', li9);
 
 }
+
+
 
 // 課題6-1 のイベントハンドラ登録処理は以下に記述
 
